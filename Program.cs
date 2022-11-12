e78a746bc767ade95f62e5579e43cb1b14bba04d
Alejandro Dávila <aled98.ad@gmail.com>
2022-11-11 21:44:12 -0600

    Just test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Tiny_TypeScript
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Putos!");
         }
     }
 }
